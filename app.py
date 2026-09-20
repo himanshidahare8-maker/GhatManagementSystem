76833e5087caa8a833d559ce5f04fb95cf48f27e
Himanshi Dahare <himanshidahare8@gmail.com>
2026-09-20 14:34:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Ghat Management System Backend is Running!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
